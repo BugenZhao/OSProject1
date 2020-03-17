7a8eb2cb5289bb8d3178646deaa1859b1d2cb8f3
Bugen <bugenzhao@qq.com>
2020-03-17 14:20:24 +0800

    Traverse tasks

--- ptree_test/jni/ptree_test.c
+++ ptree_test/jni/ptree_test.c
@@ -4,17 +4,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void traverse(struct prinfo *infos, int nr) {
+    struct prinfo **stack = malloc(sizeof(struct prinfo *) * nr);
+    int top = -1;
+    size_t tab_cnt = 0;
+    size_t i, j;
+    struct prinfo *info;
+
+    for (i = 0; i < nr; i++) {
+        info = infos + i;
+        if (top == -1) {
+            stack[++top] = info;
+        } else if (stack[top]->first_child_pid == infos[i].pid) {
+            stack[++top] = info;
+        } else if (stack[top]->next_sibling_pid == infos[i].pid) {
+            stack[top] = info;
+        } else {
+            while (stack[top]->pid != infos[i].parent_pid) --top;
+            stack[++top] = info;
+        }
+        for (j = 0; j < top; j++) printf("\t");
+        printf("%-16s,%d,%ld,%d,%d,%d,%ld\n", info->comm,
+               info->pid, info->state, info->parent_pid, info->first_child_pid,
+               info->next_sibling_pid, info->uid);
+    }
+
+    free(stack);
+}
+
 int main() {
     struct prinfo infos[MAX_PRO_CNT];
     int nr;
-    int i;
+    size_t i;
 
     syscall(__NR_ptree, infos, &nr);
     printf("nr=%d\n", nr);
 
-    for (i = 0; i < nr; i++) {
-        printf("%d, %s\n", infos[i].pid, infos[i].comm);
-    }
+    traverse(infos, nr);
 
     exit(0);
 }
